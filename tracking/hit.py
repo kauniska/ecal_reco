7a570487569d706218951ec5132d347855a77f0b
Nelson-00 <nelson.glardon@epfl.ch>
2022-10-03 16:33:16 +0200

    Update hit.py
    Creation of Hit class

--- tracking/hit.py
+++ tracking/hit.py
@@ -4,12 +4,9 @@ A hit class
 
 
 class Hit:
-    def __init__(self):
-        self.data = []
-
-    def add(self, x):
-        self.data.append(x)
-
-    def addtwice(self, x):
-        self.add(x)
-        self.add(x)
+    def __init__(self,coord,is_sidex,timestamp,timestamp_event,value):
+        self.coord = coord
+        self.is_sidex = is_sidex
+        self.timestamp = timestamp
+        self.timestamp_event = timestamp_event
+        self.value = value
