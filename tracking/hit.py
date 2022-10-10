eba8031d4c5a380cf720b8773b93a26f68b4e907
Nelson-00 <nelson.glardon@epfl.ch>
2022-10-10 13:14:14 +0200

    test_Track : use of the new constructor of the class Hit

--- tracking/hit.py
+++ tracking/hit.py
@@ -45,40 +45,6 @@ class Hit:
             self.value = args[4]
         else:
             raise ValueError("not the correct number of arguments given")
-    
-    # def __init__(self):
-    #     """
-    #     The construction is void, the parameters are set through set_parameters or set_data_frame
-    #     """
-    #     self.coord = None
-    #     self.is_sidex = None
-    #     self.timestamp = None
-    #     self.timestamp_event = None
-    #     self.value = None
-
-    # def set_parameters(self,coord,is_sidex,timestamp,timestamp_event,value):
-    #     """
-    #     Sets the parameters manually, the coordinates have to be already transformed
-    #     in the final form though the function track_reconstruction.mapping_2D
-    #     """
-    #     self.coord = coord
-    #     self.is_sidex = is_sidex
-    #     self.timestamp = timestamp
-    #     self.timestamp_event = timestamp_event
-    #     self.value = value
-
-    # def set_data_frame(self,data_frame, local_index):
-    #     """
-    #     Sets the parameters with an event of the data frame and the index of the hit local to the given event
-    #     """
-    #     self.coord = mapping_2D(data_frame['tofpet_id'][local_index],data_frame['tofpet_channel'][local_index])
-    #     if is_sidex(data_frame['tofpet_id'][local_index]):
-    #         self.is_sidex = True
-    #     else:
-    #         self.is_sidex = False
-    #     self.timestamp = data_frame['timestamp'][local_index]
-    #     self.timestamp_event = data_frame['timestamp_event']
-    #     self.value = data_frame['value'][local_index]
 
     def print(self):
         if self.is_sidex:
