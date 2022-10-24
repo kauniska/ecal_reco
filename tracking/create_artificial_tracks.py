db18f10eed54f9776fcbbb598f4d39c189b0a172
Nelson-00 <nelson.glardon@epfl.ch>
2022-10-24 10:04:10 +0200

    create_artificial_tracks
    method to create list of hits based on parameters t and x0

---
+++ tracking/create_artificial_tracks.py
@@ -0,0 +1,11 @@
+import hit
+import numpy as np
+def create_artificial_tracks(t,x0,sampling=1):
+    """ Takes a tangent and offset as parameters
+        returns a list of hits along the track defined by t and x0
+        sampling is between 0 and 1 and corresponds to the probability with which a hit 
+        trough which the track goes is present in the final list 
+    """
+    rand = np.random
+    hits = 
+    return hits
