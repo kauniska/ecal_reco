0c7d0914baaf558c1e9fdf465debf0195eced178
Nelson-00 <nelson.glardon@epfl.ch>
2023-01-04 14:55:48 +0100

    Test of the effect of the geometry
    The code of test_hough_transformsi.iypnb was completed to test the effect of the geometry

--- tracking/artificial_tracks.py
+++ tracking/artificial_tracks.py
@@ -8,12 +8,15 @@ sys.path.insert(1, r"C:\Users\nelg\Desktop\Cours\Labo\TP4\Git\utils")
 from parameters import *
 from physics import overlap_length
 
+# This function is deprecated : it relies on the fitting algorithm to create the hits. It should not be used
+'''
 def create_artificial_tracks(t,x0,sampling=1):
     """ Takes a tangent and offset as parameters
         returns a list of hits along the track defined by t and x0
         sampling is between 0 and 1 and corresponds to the probability with which a hit 
         trough which the track goes is present in the final list 
     """
+
     hits = []
     traj = Track()
     traj.t = t
@@ -38,8 +41,11 @@ def fit_artificial_track(t,x0,sampling=1):
     hits = create_artificial_tracks(t,x0,sampling)
     fit_track = Track(hits)
     return fit_track.t, fit_track.x0
+'''
 
-
+# This function remplaces with the previous function create_artificial_tracks which didn't take into 
+# account the geometry of the detector. It should also be used to create the hits that will be used to
+# test the track reconstruction without the geoetrical considerations since r
 def create_artificial_tracks_geom(t,x0,sidex,sampling=1):
     ''' 
     Takes a tangent and offset as parameters
@@ -52,9 +58,9 @@ def create_artificial_tracks_geom(t,x0,sidex,sampling=1):
 
     tol = 10**(-10)
 
-    xmin = min(x0,2*t*n_layers*(thickness+thickness_screen)+x0)
+    xmin = min(x0,t*total_height+x0)
     xmin = max(xmin,0)
-    xmax = max(x0,2*t*n_layers*(thickness+thickness_screen)+x0)
+    xmax = max(x0,t*total_height+x0)
     xmax = min(xmax,n_strips*width)
 
     coord_x_min = round(xmin//width + 1)
@@ -68,10 +74,55 @@ def create_artificial_tracks_geom(t,x0,sidex,sampling=1):
     for j in range(coord_x_min,coord_x_max+1):
         if t == 0:
             z_min = 0
-            z_max = 2*n_layers*(thickness+thickness_screen)
+            z_max = total_height
         else:
             z_max = max((coord_to_pos_x(j)-width/2-x0)/t,(coord_to_pos_x(j)+width/2-x0)/t)
             z_min = min((coord_to_pos_x(j)-width/2-x0)/t,(coord_to_pos_x(j)+width/2-x0)/t)
+        for i,z in enumerate(z_i):
+            if overlap_length(z,[z_min,z_max]) > tol:
+                rand = np.random.uniform()
+                if sampling>=rand:
+                    hits.append(Hit([j,i+1],sidex,0,0,0))
+
+    return hits
+
+
+# This function creates the track with the same conventions used in the algorithm without geometry. This causes a lot of non-concerned hits
+# to be fired, since the cells are considered to have a height of 2cm instead of 0.4cm. This should thus not be used.
+def create_artificial_tracks_no_geom(t,x0,sidex,sampling=1):
+    ''' 
+    Takes a tangent and offset as parameters
+    returns a list of hits on the plane x-z if sidex, y-z otherwise, along the track defined by t and x0.
+    In this function, x0 is the offset at the top of the detector, not the bottom
+    sampling is between 0 and 1 and corresponds to the probability with which a hit 
+    trough which the track goes is present in the final list 
+    '''
+
+    hits = []
+
+    tol = 10**(-10)
+    x0 -= width
+
+    xmin = min(x0,x0-t*total_height)
+    xmin = max(xmin,0)
+    xmax = max(x0,x0-t*total_height)
+    xmax = min(xmax,n_strips*width)
+
+    coord_x_min = round(xmin//width + 1)
+    coord_x_max = round(xmax//width + 1)
+
+    z_i = []
+    for i in range(n_layers):
+        z_i.append([i*Delta_z, (i+1)*Delta_z])
+
+    for j in range(coord_x_min,coord_x_max+1):
+        if t == 0:
+            z_min = 0
+            z_max = total_height
+        else:
+            #j'en suis ici, revoir dessin sur feuille de brouillon et adapter pour x0 top 
+            z_max = max((coord_to_pos_x(j) - width/2 - x0)/t + total_height, (coord_to_pos_x(j) + width/2 - x0)/t + total_height)
+            z_min = min((coord_to_pos_x(j) - width/2 - x0)/t + total_height, (coord_to_pos_x(j) + width/2 - x0)/t + total_height)
         for i,z in enumerate(z_i):
             if overlap_length(z,[z_min,z_max]) > tol:
                 rand = np.random.uniform()
--- tracking/other_hough_transform.py
+++ tracking/other_hough_transform.py
@@ -6,13 +6,9 @@ import sys
 sys.path.insert(1, r"C:\Users\nelg\Desktop\Cours\Labo\TP4\Git\utils")
 from parameters import *
 
-# event = df_hits.iloc[interesting_events[600]]
+# Test version of Hough transform : discretization of the Hough space.
+# This algorithm is slow but allows to visualize what happens in Hough space
 def new_method_tracks(hits):
-    # hits = []
-    # for i in range(event['n_hits']):
-    #     h = Hit(event,i)
-    #     if h.is_sidex:
-    #         hits.append(h)
     n_points = 100
     tmax = 5
 
@@ -41,24 +37,13 @@ def new_method_tracks(hits):
     index_max_tx = np.where(map == map.max())
     index_max_x = round(np.sum(index_max_tx[0])/len(index_max_tx[0]))
     index_max_t = round(np.sum(index_max_tx[1])/len(index_max_tx[1]))
-    # plt.plot(index_max_t,index_max_x,'rx')
 
     x0 = x[index_max_x]
     t = T[index_max_t]
 
-    # detector_map = np.zeros((8,24))
-    # for h in hits:
-    #     detector_map[8-h.coord[1]][h.coord[0]-1] += 1
-
-    # z_grid = np.linspace(0,2*8,10)
-    # x_track = t*(z_grid-16)+x0-1.6
-
-    # plt.figure()
-    # plt.imshow(detector_map, interpolation='nearest', extent=[0,24*1.6,0,8*2])
-    # plt.plot(x_track,z_grid,'r-')
-    # plt.show
     return t,x0
 
+# Previous version of Hough transform with the geometrical adaptation : precise but slow
 def old_method_tracks(hits):
     n_points=100
     max=5 # => angle scanning between [-78.7°,78,7°] 
@@ -79,6 +64,45 @@ def old_method_tracks(hits):
         x0['xu'][i][n_points:2*n_points] = pos[0] + width/2 - (pos[1] - thickness/2)*tpos # Up boundary for x0 for hit number hit in region t>0
         x0['xd'][i][0:n_points] = pos[0] - width/2 - (pos[1] - thickness/2)*tneg
         x0['xd'][i][n_points:2*n_points] = pos[0] - width/2 - (pos[1] + thickness/2)*tpos
+    ##### Now have to find the region of overlap
+
+    T=np.append(tneg,tpos)
+    t_overlap=[[] for i in range(2*n_points)]
+    boundaries=[[] for i in range(2*n_points)]
+    overlap=0
+    for t in range(2*n_points):
+        a=0
+        t_overlap[t],a, boundaries[t]=max_overlap(x0,t)
+        if a>overlap:
+            overlap=a
+    t_max_overlap=[T[t] for t in range(2*n_points) if len(t_overlap[t])==overlap]
+    min_max_overlap=[boundaries[t] for t in range(2*n_points) if len(t_overlap[t])==overlap]
+    index_=[t_overlap[t] for t in range(2*n_points) if len(t_overlap[t])==overlap][0]
+    t=np.mean(t_max_overlap)
+    out=np.mean([np.mean(ov) for ov in min_max_overlap])
+    return out, t, index_
+
+# Previous version of the algorithm without the geometrical adaptation, 2 cells plus 2 passive layers are
+# merged together -> information on the vertical confinment is lost
+def old_method_tracks_no_geom(hits):
+    n_points=100
+    max=5 # max=5 => angle scanning between [-78.7°,78,7°] 
+    tneg=np.linspace(-max,0,n_points) # region over which we want to look for the angle
+    tpos=np.linspace(0,max,n_points)
+    n_hits=len(hits)
+    # x0=pd.DataFrame(columns=['xu','xd'])
+    # x0['xu']=[np.zeros(2*n_points) for i in range(n_hits)]
+    # x0['xd']=[np.zeros(2*n_points) for i in range(n_hits)]
+    x0=pd.DataFrame(data = {
+        'xu':[np.zeros(2*n_points) for i in range(n_hits)],
+        'xd': [np.zeros(2*n_points) for i in range(n_hits)]
+        })
+   
+    for hit in range(0,n_hits):
+        x0['xu'][hit][0:n_points]=(hits[hit][0]+1)*1.6-(hits[hit][1]-8)*2*tneg # Up boundary for x0 for hit number hit in region t<0
+        x0['xu'][hit][n_points:2*n_points]=(hits[hit][0]+1)*1.6-(hits[hit][1]-9)*2*tpos # Up boundary for x0 for hit number hit in region t>0
+        x0['xd'][hit][0:n_points]=hits[hit][0]*1.6-(hits[hit][1]-9)*2*tneg
+        x0['xd'][hit][n_points:2*n_points]=hits[hit][0]*1.6-(hits[hit][1]-8)*2*tpos    
     ##### Now have to find the region of overlap
 
     T=np.append(tneg,tpos)
