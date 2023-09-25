37d125cbc585f8f56173cab20af52b2778ce4071
scalpute <pascal.kolly2@gmail.com>
2023-09-25 16:52:37 +0200

    Test import run_0000.root and understand its structure

--- test_import_root/test_import.py
+++ test_import_root/test_import.py
@@ -2,11 +2,26 @@ import pandas as pd
 import uproot
 #import uproot3
 import numpy as np
+import matplotlib.pyplot as plt
 
 br_list_data = ['n_hits', 'tofpet_id', 'tofpet_channel', 'timestamp', 't_coarse', 't_fine', 'timestamp', 'v_coarse', 'v_fine', 'value']
 br_list_evt = ['timestamp', 'evt_number', 'flags']
 evt_tree = 'event'
 hits_tree = 'board_57'
 
-with uproot.open('C:\\Users\\Pascal\\Desktop\\TP4a\\git\\test_data_loading\\data_0000.root') as h:
-    h['event_data'].plot()
+data = uproot.open('C:\\Users\\Pascal\\Desktop\\TP4a\\git\\test_data_loading\\data_0000.root')
+
+x = data['event_data']['evt_number']
+y = data['event_data']['timestamp']
+print()
+print(data['event_data'].keys())
+print(x)
+fig, ax = plt.subplots()
+ax.plot([],'+')
+
+ax.set(xlabel='x', ylabel='voltage (mV)',
+       title='test')
+ax.grid()
+
+fig.savefig("test.png")
+plt.show()
