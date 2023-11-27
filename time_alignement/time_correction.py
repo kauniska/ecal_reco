d4dda611939aa670f2cf5fc4734e0c2ac3402616
eliornand <eliot.bornand@epfl.ch>
2023-11-27 11:05:04 +0100

    time corrections functions

--- time_alignement/time_correction.py
+++ time_alignement/time_correction.py
@@ -5,21 +5,36 @@ import fnmatch
 sys.path.insert(1, r'C:\Users\eliot\EPFL\TP4_ECAL\Code\ecal_reco\utils')
 sys.path.insert(1, r'C:\Users\eliot\EPFL\TP4_ECAL\Code\ecal_reco\tracking')
 from track import Track
+from track_reconstruction import *
 from track3D import Track3D
 
-def time_correction(T3D):
+def time_correction_fiber(args):
     Speed_In_Fiber = 15 # cm/ns
-    Tx = T3D.x
-    Ty = T3D.y
+
+    #If one argument whcih is Track3D, change the timestamp of each hits of the track and return the track
+    if len(args)== 1 : 
+        Tx = args[0].x
+        Ty = args[0].y
+        
+        newx = []
+        newy = []
+        for h in Tx.hits:
+            newx.append(h)
+            newx[-1].timestamp = h.timestamp - Ty.x(h.get_pos()[1])/Speed_In_Fiber
+        for h in Ty.hits:
+            newy.append(h)
+            newy[-1].timestamp = h.timestamp - Ty.x(h.get_pos()[1])/Speed_In_Fiber
+        Txprime = Track(newx)
+        Typrime = Track(newy)
+        return Track3D(Txprime,Typrime)
     
-    newx = []
-    newy = []
-    for h in Tx.hits:
-        newx.append(h)
-        newx[-1].timestamp = h.timestamp - Ty.x(h.get_pos()[1])/Speed_In_Fiber
-    for h in Ty.hits:
-        newy.append(h)
-        newy[-1].timestamp = h.timestamp - Ty.x(h.get_pos()[1])/Speed_In_Fiber
-    Txprime = Track(newx)
-    Typrime = Track(newy)
-    return Track3D(Txprime,Typrime)
+    # If 2 arguments : a timestamp, a x/y coordinate(in physical units [m]))
+    if len(args) == 2 :
+        timestamp = args[0] - args[1]/Speed_In_Fiber
+
+        return timestamp
+    
+    # If 3 arguments : A timestamp, and coordinates (in tofpet id and chanels), change the timestamp and return it
+    if len(args) == 3 :
+        timestamp = args[0] - mapping_2D(args[1],args[2])[1]/Speed_In_Fiber
+        return timestamp
--- utils/parameters.py
+++ utils/parameters.py
@@ -22,6 +22,10 @@ total_height = 2*n_layers*(thickness+thickness_screen)
 #Distance between 2 active layer of the same orientation
 Delta_z = 2*(thickness+thickness_screen)
 
+#Distance on the electronic board between each channel's detector and the central tofpet 
+distance_Channels_X = np.nan_to_num(np.ndarray(shape=(8,64), dtype=float), nan=0, posinf=0, neginf=0)*0
+distance_Channels_Y = np.nan_to_num(np.ndarray(shape=(8,64), dtype=float), nan=0, posinf=0, neginf=0)*0
 
-
-
+#General offset on time computed with time alignement algorithm, for each channel
+muX = np.nan_to_num(np.ndarray(shape=(8,64), dtype=float), nan=0, posinf=0, neginf=0)*0
+muY = np.nan_to_num(np.ndarray(shape=(8,64), dtype=float), nan=0, posinf=0, neginf=0)*0
